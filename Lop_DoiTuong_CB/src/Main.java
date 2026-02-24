e7acbd496e80da47af0f75ee55a831f681b355f2
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-02-24 21:35:05 +0700

    OOP_CB

---
+++ Lop_DoiTuong_CB/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SinhVien sv = new SinhVien();
+		sv.mssv = "66132390";
+		sv.ten = "Nguyen";
+		sv.tuoi = 19;
+		sv.hocbai("Lập trình Java");
+	}
+
+}
---
+++ Lop_DoiTuong_CB/src/SinhVien.java
@@ -0,0 +1,8 @@
+public class SinhVien {
+	int tuoi;
+	String mssv, ten;
+	void hocbai(String monhoc)
+	{
+		System.out.println("Bạn " + ten + " có mssv là " + mssv + " đang học môn " + monhoc);
+	}
+}
