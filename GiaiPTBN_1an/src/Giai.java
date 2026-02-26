31e1c390c0f86aa89b119215e8e6fb7cad285a20
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-02-26 20:28:27 +0700

    PTBN_1an

---
+++ GiaiPTBN_1an/src/Giai.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Giai {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// PTBN1an co dang ax + b = 0;
+		// khai bao lop scanner
+		Scanner sc = new Scanner(System.in);
+		float a, b;
+		a = sc.nextFloat();
+		b = sc.nextFloat();
+		if(a == 0) System.out.println("Phuong trinh nay co vo so nghiem");
+		else System.out.println("Phuong trinh co nghiem duy nhat x = -" + b + " \\ " + a + " = " + -(b/a));
+	}
+
+}
