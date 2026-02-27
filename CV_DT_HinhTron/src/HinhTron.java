9b8117454fc2063d0d287bed19ed32fd234c5047
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-02-27 20:51:42 +0700

    CV_DThinhtron

---
+++ CV_DT_HinhTron/src/HinhTron.java
@@ -0,0 +1,12 @@
+
+public class HinhTron {
+	double bankinh;
+	double chuviHT(double bankinh)
+	{
+		return 2 * bankinh * 3.14;
+	}
+	double dientichHT(double bankinh)
+	{
+		return bankinh * bankinh * 3.14;
+	}
+}
---
+++ CV_DT_HinhTron/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		HinhTron tron = new HinhTron();
+		tron.bankinh = sc.nextDouble();
+		double cv = tron.chuviHT(tron.bankinh), dt = tron.dientichHT(tron.bankinh);
+		System.out.println("Chu vi hinh tron la " + cv);
+		System.out.println("Dien tich hinh tron la " + dt);
+	}
+
+}
