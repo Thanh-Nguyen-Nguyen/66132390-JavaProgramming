6d9d13284385ab044c46a5b8c5e8bb1d6d3a9311
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-02-28 18:34:04 +0700

    Update doi.java

--- quydoi_doC_doF/src/quydoi_doC_doF/doi.java
+++ quydoi_doC_doF/src/quydoi_doC_doF/doi.java
@@ -7,6 +7,7 @@ public class doi {
 		Scanner inp = new Scanner(System.in);
 		double doC = inp.nextDouble();
 		System.out.println(doC + " do C " + "= " + ((doC * 1.8) + 32) + " do F");
+		inp.close();
 	}
 
 }
