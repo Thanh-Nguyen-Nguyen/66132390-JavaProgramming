35018297bc8c4ab5f2fda9f4e766f0e1837f8b12
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-02-28 18:33:37 +0700

    Create doi.java

---
+++ quydoi_doC_doF/src/quydoi_doC_doF/doi.java
@@ -0,0 +1,12 @@
+package quydoi_doC_doF;
+import java.util.Scanner;
+public class doi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inp = new Scanner(System.in);
+		double doC = inp.nextDouble();
+		System.out.println(doC + " do C " + "= " + ((doC * 1.8) + 32) + " do F");
+	}
+
+}
