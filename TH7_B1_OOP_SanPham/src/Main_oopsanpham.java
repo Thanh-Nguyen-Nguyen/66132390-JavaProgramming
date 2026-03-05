c116acbeee202e800e1f578704f4f1d133523b9b
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-03-05 21:16:09 +0700

    Update Main_oopsanpham.java

--- TH7_B1_OOP_SanPham/src/Main_oopsanpham.java
+++ TH7_B1_OOP_SanPham/src/Main_oopsanpham.java
@@ -3,7 +3,8 @@ public class Main_oopsanpham {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Sanpham sp = new Sanpham("SP01", "Laptop", 1500000, 2);
+		Sanpham sp = new Sanpham("SP01", "Laptop", 150000, 2);
+		sp.setSoLuong(7);
         sp.hienThiThongTin();
 	}
 
