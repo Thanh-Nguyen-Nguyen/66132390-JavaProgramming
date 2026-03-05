0b3dafa56cc56970975db8f36c47c72d5bc0d7d1
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-03-05 21:15:11 +0700

    ham main

---
+++ TH7_B1_OOP_SanPham/src/Main_oopsanpham.java
@@ -0,0 +1,10 @@
+
+public class Main_oopsanpham {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sanpham sp = new Sanpham("SP01", "Laptop", 1500000, 2);
+        sp.hienThiThongTin();
+	}
+
+}
