65903d5c65d1795b34da0621a7cbfd32c0d2509c
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-03-18 04:00:23 +0700

    tao class sinhvien

---
+++ Bai11_OOP_QLSV/src/SinhVien/sinhvien.java
@@ -0,0 +1,18 @@
+package SinhVien;
+public class sinhvien {
+	private String maSV;
+    private String hoTen;
+    private double diemTB;
+    public sinhvien(String maSV, String hoTen, double diemTB) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diemTB = diemTB;
+    }
+    public String getMaSV() { return maSV; }
+    public String getHoTen() { return hoTen; }
+    public double getDiemTB() { return diemTB; }
+    public String toString() {
+        String res = "Ban: " + hoTen + " co mssv la " + maSV + " co diem trung binh la " + diemTB;
+        return res;
+    }
+}
