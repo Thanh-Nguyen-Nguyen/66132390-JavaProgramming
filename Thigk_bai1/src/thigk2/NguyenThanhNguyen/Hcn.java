ce5ea6ea40559d2f9ff80cced0502d8500b32023
nguyen <nguyen.nth.66cntt@ntu.edu.vn>
2026-04-23 00:34:03 +0700

    Lần thứ 2

--- Thigk_bai1/src/thigk2/NguyenThanhNguyen/Hcn.java
+++ Thigk_bai1/src/thigk2/NguyenThanhNguyen/Hcn.java
@@ -1,5 +1,38 @@
 package thigk2.NguyenThanhNguyen;
 
 public class Hcn {
-
+	private double x1, y1, x2, y2;
+	public Hcn()
+	{
+		
+	}
+	public Hcn(double x1, double y1, double x2, double y2)
+	{
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+	public double getX1() {return x1;}
+	public double getX2() {return x2;}
+	public double getY1() {return y1;}
+	public double getY2() {return y2;}
+	public void setX1(double x1) {this.x1 = x1;}
+	public void setX2(double x2) {this.x2 = x2;}
+	public void setY1(double y1) {this.y1 = y1;}
+	public void setY2(double y2) {this.y2 = y2;}
+	public double CV()
+	{
+		return 2 * (x2 - x1 + y2 - y1); 
+	}
+	public double DT()
+	{
+		return (x2 - x1) * (y2 - y1); 
+	}
+	public String toString()
+	{
+		return ("Goc tren trai co toa do la" + "(" + x1 + " " + y1 + ")" + 
+				"Goc duoi phai co toa do la" + "(" + x2 + " " + y2 + ")"
+				);
+	}
 }
--- Thigk_bai1/src/thigk2/NguyenThanhNguyen/Main.java
+++ Thigk_bai1/src/thigk2/NguyenThanhNguyen/Main.java
@@ -1,10 +1,26 @@
 package thigk2.NguyenThanhNguyen;
-
+import java.util.Scanner;
+// cach giai: Tao class Hcn gom 4 thuoc tinh x1, x2, y1, x2; Constructor va cac phuong thuc getter, setter va toString
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		double x1, y1, x2, y2;
+		System.out.println("Nhap toan do goc tren trai :");
+		System.out.print("x1 = ");
+		x1 = sc.nextDouble();
+		System.out.print("y1 = ");
+		y1 = sc.nextDouble();
+		System.out.println("Nhap toan do goc duoi phai :");
+		System.out.print("y1 = ");
+		x2 = sc.nextDouble();
+		System.out.print("y2 = ");
+		y2 = sc.nextDouble();
+		Hcn Hcn1 = new Hcn(x1, y1, x2, y2);
+		sc.close();
+		double cv = Hcn1.CV();
+		
 	}
 
 }
